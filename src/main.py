80e6f20bdb36f861b02f346cd16b452ef0ce9458
Yeti <88898324+Delation@users.noreply.github.com>
2022-05-04 11:03:16 -0500

    Implement #1 and #3
    (Further changes still planned)

--- src/main.py
+++ src/main.py
@@ -5,45 +5,103 @@ import enum
 
 Entities = enum.Enum('Entities', 'human')
 BaseStats = {
-	Entities.human: lambda object : [ i[0](i[1]) for i in ( (object.setHP,20), (object.setMP,5), (object.setATK,10), (object.setDEF,5), (object.setINT,30), (object.setSPD,2), (object.setLUC,0) ) ],
+	Entities.human: lambda object : [ i[0](i[1]) for i in ( (object.setHP,20), (object.setMP,5), (object.setATK,10), (object.setINT,30), (object.setSPD,2), (object.setLUC,0), ) ],
 }
-Tiles = enum.Enum('Tiles', 'grass dirt stone air')
+Tiles = enum.Enum('Tiles', 'air grass dirt stone')
 Levels = (
 	{
 		'name': 'Tutorial',
-		'map': {
-			'size': (10, 5),
-			'data': (
-				
-			)
-		},
-		'data': (
-			[ Tiles.stone for i in range(10) ],
+		'maps': (
+			{
+				'id': 0,
+				'data': lambda map : [ i[0](**i[1]) for i in ( (map.setTile, {'x': 0, 'y': 0, 'tile': Tiles.grass,}), ) ],
+			},
 		),
+		'mapIndex': 0,
 		'uuid': uuid.uuid4(),
 	},
 )
 Classes = {
-	#'Melee': lambda#
-	#''
+	'Melee': lambda a : a+b,
+	'Ranger': lambda a : a+b,
+	'Mage': lambda a : a+b,
+	'Support': lambda a : a+b,
 }
 
 sleep = 0.05
 punctuation = '?!.,;:-'
 
+def clear():
+	print('\x1B[2J\x1B[H')
+
 def type(input:str = '', multiplier = 1, end = '\n') -> None:
+	if multiplier < 0:
+		print(input, end = end)
+		return
 	for i in input:
 		sys.stdout.write(i)
 		sys.stdout.flush()
 		time.sleep(sleep / multiplier)
 		if i in punctuation or i == '\n':
-			time.sleep(sleep / multiplier * 10)
+			time.sleep(sleep / multiplier * 6)
 	print('', end = end)
 	return
 
-def rInput(inp:str = '', multiplier = 1, end = '\n'):
+def rInput(inp:str = '', multiplier = 1, end = '\n') -> str:
 	type(inp, multiplier, end)
 	return input()
+
+class Map():
+	def __init__(self, id:int):
+		self.id = id
+		self.uuid = uuid.uuid4()
+		self.data = []
+	
+	# Map().setSize() may or may not be implemented in the future
+	#def setSize(self, **args) -> None:
+		#self.size = args
+		## Note: This does not validate the dict before passing it, so     #
+		## please make sure you do not pass anything other than the below  #
+		## dict: {'x': x, 'y': y}. The origin will always remain (0,0)     #
+		#return
+	
+	def setTile(self, **args) -> None:
+		self.data.append(args)
+		# Note: If you are creating your own levels, make sure it follows #
+		# the guidelines, because Map().setTile() has no checks, so the   #
+		# program will crash upon read failure.                           #
+		return
+	
+	def showMap(self, user) -> str:
+		text = ''
+		for y in range(user.y - 2, user.y + 3):
+			for x in range(user.x - 2, user.x + 3):
+				hit = '_'
+				for tile in self.data:
+					if user.x == x and user.y == y:
+						hit = '+'
+					elif tile['x'] == x and tile['y'] == y:
+						hit = '#'
+				text = text + '|' + hit + '|'
+			text = text + '\n'
+		return text
+		
+
+class Coordinates():
+	def __init__(self, x:int = 0, y:int = 0):
+		self.setCoordinates(x = x, y = y)
+
+	def setCoordinates(self, **coordinates) -> list:
+		x = coordinates.get('x')
+		y = coordinates.get('y')
+		if not x is None:
+			self.x = x
+		if not y is None:
+			self.y = y
+		return x, y
+	
+	def checkCoordinates(self, x:int, y:int) -> bool:
+		return self.x == x and self.y == y
 
 class Stats():
 	def __init__(self):
@@ -53,7 +111,6 @@ class Stats():
 		
 		# Individual abilites
 		self.setATK(0)
-		self.setDEF(0)
 		self.setINT(0)
 		self.setSPD(0)
 		self.setLUC(0)
@@ -72,10 +129,6 @@ class Stats():
 		self.ATK = int(attack)
 		return self.ATK
 	
-	def setDEF(self, defense:int) -> int:
-		self.DEF = int(defense)
-		return self.DEF
-	
 	def setINT(self, intelligence:int) -> int:
 		self.INT = int(intelligence)
 		return self.INT
@@ -92,13 +145,14 @@ class Stats():
 	def getStats(self):
 		return '\n'.join([ '%s: %s' % (i, self.__dict__[i]) for i in self.__dict__.keys() if i in dir(Stats()) ])
 
-class Entity(Stats):
+class Entity(Stats, Coordinates):
 	def __init__(self):
 		super().__init__()
 		# Identification
 		self.setName('')
 		self.uuid = uuid.uuid4()
 		self.setEntity(Entities.human)
+		self.setCoordinates(x = 0, y = 0)
 	
 	def setName(self, name:str) -> str:
 		self.name = str(name)
@@ -125,21 +179,42 @@ class Level():
 		self.uuid = level['uuid']
 		
 		# Map
-		self.map = level['map']
-		
-		# Data
-		self.data = level['data']
-
-def main():
-	user = Player()
-	name = rInput('What\'s your name, young one?', 2, ' ')
-	user.setName(name)
-	#type('Ah, so your name is %s. Never come near me or my family ever again.' % user.name, 2)
+		self.maps = [ Map(map['id']) for map in level['maps'] ]
+		[ level['maps'][i]['data'](self.maps[i]) for i in range(len(level['maps'])) ]
+		self.mapIndex = level['mapIndex']
 	
-	type('Your stats:\n\n%s\n' % user.getStats().replace('\n','\n'*2), 30)
+	def showMap(self, user:Player) -> str:
+		return self.maps[self.mapIndex].showMap(user)
+
+def main() -> None:
+	user = Player()
+	name = rInput('What\'s your name, young one?', 1, ' ')
+	user.setName(name)
+	type('Ah, so your name is %s. I\'ve heard very much about you.\nYes, very much indeed.' % user.name, 1)
+	
+	rInput('Press enter to continue!', 2)
 	
 	level = Level()
-	print('--- %s LEVEL ---\n' % level.name)
+	next = ''
+	while True:
+		clear()
+		print('--- %s LEVEL ---\nRoom: %s' % (level.name, level.maps[level.mapIndex].id))
+		input = rInput(level.showMap(user) + ('' if not next else '\n%s\n' % next), -1, '>>> ').lower()
+		next = ''
+		if input == 'up':
+			user.y -= 1
+		elif input == 'down':
+			user.y += 1
+		elif input == 'left':
+			user.x -= 1
+		elif input == 'right':
+			user.x += 1
+		elif input == 'stats':	
+			next = 'Your stats:\n\n%s\n' % user.getStats().replace('\n','\n'*2)
+		elif input == 'exit':
+			return
+		else:
+			next = 'Type \'help\' to see all the available commands!'
 	
 if __name__ == '__main__':
 	main()
